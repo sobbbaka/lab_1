a0e28f660e528990e61b4cba9ee16abfbae4f4d3
Denis Levkov <denlevakov54@gmail.com>
2023-09-18 10:52:23 +0700

    slil

--- 123.py
+++ 123.py
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 print("Hello world")
-=======
 
 f = open('xyz.txt','w')  # открытие в режиме записи
 f.write('Hello World')  # запись Hello World в файл
 f.close()  # закрытие файла
->>>>>>> bugFix
